c82119e9d0ead3b8ee6be2d523f61c3aee2b3592
365462 <ss.lorena1495@gmail.com>
2017-11-21 11:31:58 -0500

    javascript terminado

--- js/app.js
+++ js/app.js
@@ -1,58 +1,106 @@
-/*Seleccion de atributos dentro de la etiqueta main*/
-var container = document.getElementById('container');
-var boxList = document.getElementsByClassName('box-list')[0];
+window.addEventListener('load', begin);
 
-/*Funciones creadas*/
+var addList = document.getElementById('addList');
+var form = document.getElementById('form');
+var listName = document.getElementById('name-list');
+var button = document.getElementById('button-id');
 
-function createList(event) {
-  //creas las etiquetas "form", "input" y "a"
-  var form = document.createElement('form');
-  var input = document.createElement('input');
-  var button = document.createElement('a');
-
-  //dentro del contenedor le agregas la etiqueta form
-  container.appendChild(form);
-
-  //dentro de la etiqueta form le agregas las etiquetas input y buttom
-  form.appendChild(input);
-  form.appendChild(button);
-
-  //al input le agregas un atributo llamado "placeholder"
-  //es una descripcion de lo que quieres agregar al input
-  input.setAttribute('placeholder','Añadir una lista ...')
-
-  //El boton llevará el nombre de "guardar"
-  button.textContent = 'Guardar';
-  event.preventDefault();
-  container.removeChild(boxList);
-
-  button.addEventListener('click', function(event) {
-    event.preventDefault();
-    tarea();
-    function tarea(){
-      var container2 = document.createElement('div');
-      var captureText = input.value;
-      container2.textContent=captureText;
-      container.appendChild(container2);
-
-      var addList = document.createElement('p');
-      addList.textContent='Añadir una tarea';
-      container.appendChild(addList);
-
-      addList.addEventListener('click', function(event){
-          event.preventDefault();
-          var container2 = document.createElement('div');
-          var captureText = input.value;
-          container2.textContent=captureText;
-          container.appendChild(container2);
-
-          var addList = document.createElement('p');
-          addList.textContent='Añadir una tarea';
-          container.appendChild(addList);
-      })
-    }
-  })
+function newList(event) {
+  event.target.classList.toggle('display-none');
+  form.classList.toggle('display-none');
 }
 
-/*Llamado a los eventos*/
-boxList.addEventListener('click',createList);
+function createList(event) {
+  event.preventDefault();
+
+  if(listName.value) {
+    form.classList.toggle('display-none');
+
+    var title = listName.value.toUpperCase();
+    var titleElement = document.createElement('div');
+
+    titleElement.textContent = title;
+
+    form.parentElement.insertBefore(titleElement, form);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "#");
+
+    var linkText = document.createTextNode("Añadir una tarea...");
+    link.appendChild(linkText);
+
+    link.addEventListener("click", newTask);
+    titleElement.parentElement.appendChild(link);
+
+    var containerRight = document.createElement('div');
+    containerRight.classList.add('container-form');
+    form.parentElement.parentElement.appendChild(containerRight);
+    containerRight.appendChild(addList);
+    addList.classList.toggle('display-none');
+
+    containerRight.insertBefore(form, containerRight.firstElementChild);
+
+    listName.value = '';
+  }
+}
+
+function newTask(event) {
+  event.preventDefault();
+
+  // oculta el link
+  event.target.classList.toggle('display-none');
+
+  var subForm = document.createElement('form');
+  event.target.parentElement.appendChild(subForm);
+
+  var taskName = document.createElement('textarea');
+  taskName.classList.add('name-list');
+  subForm.appendChild(taskName);
+
+  var taskBtn = document.createElement('button');
+  taskBtn.setAttribute('type', 'submit');
+  taskBtn.textContent = 'Añadir';
+  taskBtn.classList.add('button-id');
+  taskBtn.disabled = true;
+
+  taskName.addEventListener('keyup', enableButton);
+
+  subForm.appendChild(taskBtn);
+
+  taskName.focus();
+
+  taskBtn.addEventListener('click', createTask);
+}
+
+function enableButton(event) {
+  var text = event.target.value.trim();
+
+  if(text.legth)
+    event.target.nextElementSibling.disabled = true;
+  else
+  event.target.nextElementSibling.disabled = false;
+}
+
+function createTask(event) {
+  event.preventDefault();
+
+  var textDiv = event.target.previousSibling.value.trim();
+  var div = document.createElement('div');
+
+  div.textContent = textDiv;
+
+  var parentButton = event.target.parentElement;
+
+  parentButton.classList.toggle('display-none');
+  parentButton.parentElement.appendChild(div);
+
+  div.parentElement.appendChild(parentButton.previousElementSibling);
+
+  // muestra el link, para desplegar el formulario
+  parentButton.parentElement.lastElementChild.classList.toggle('display-none');
+}
+
+function begin() {
+  addList.addEventListener('click', newList);
+  button.addEventListener('click', createList);
+}
